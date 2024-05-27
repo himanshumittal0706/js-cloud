382a2e374ebe88079b2ebb92f1b1a38fd4a2b5b9
Himanshu Mittal <117907145+himanshumittal0706@users.noreply.github.com>
2024-05-27 11:35:18 +0000

    datatypes discuss

---
+++ basicsjs/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as a newer version;
+
+// alert("Hello cloud") not run in nodejs;
+
+let name = "Himanshiu" 
+let age = 29;
+let isLoggedin = false
+let state;
+
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/false 
+// null // standelon value
+// undefined => someone value is not defind
+// symbol = unique
+
+// object 
+
+console.log(typeof "Himanshiu");
+console.log(typeof age);
+console.log(typeof null); // take out the output object
+console.log(typeof undefined); // take out the output undefinrd
+
+
+
+
+
--- basicsjs/test.js
+++ basicsjs/test.js
@@ -1,10 +1,22 @@
+const accountID = 14453
+let useremail = "cloud009@gmail.com"
+var userpassword = "krishna008"
+accountCity = "jaipur"
 
-const Hari = "Hai shri Radha Krishna";
-console.log(Hari);
 
+// accountID = 2; // this syntax is not allowed
+useremail = "hc009@gmail.com"
+userpassword = "7812346"
+accountCity = "indore"
 
+let accountState;
 
+/*
+Prefer not to use var because of block Sope and functional scope
+*/
 
+console.log(useremail)
+console.table([accountID, useremail, userpassword, accountCity, accountState]);
 
 
 
